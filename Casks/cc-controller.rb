ae0bc08c8e6538231ab6f42f863c5b6306ad4173
CC Controller CI <ci@kitifica.com>
2026-08-14 14:40:44 +0000

    chore: bump cc-controller to v1.4.6

--- Casks/cc-controller.rb
+++ Casks/cc-controller.rb
@@ -1,14 +1,14 @@
 cask "cc-controller" do
-  version "1.4.5"
+  version "1.4.6"
 
   on_arm do
-    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.4.5/CC.Controller-1.4.5-arm64.dmg"
-    sha256 "7b7d98a1959fdc8c39d65a5abe8973438c119ed0efe8d0c0af07325b1d3cbd30"
+    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.4.6/CC.Controller-1.4.6-arm64.dmg"
+    sha256 "4841bd2243f341845811dcb23255427534c7089c9472bf2f33561ca508bba348"
   end
 
   on_intel do
-    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.4.5/CC.Controller-1.4.5.dmg"
-    sha256 "ed8ebb24f15343fe1549b52ad899b6b75a207f2fd1debc7793c3f796f75c3473"
+    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.4.6/CC.Controller-1.4.6.dmg"
+    sha256 "b5951aaaab4cb52abf55e0d61759c1cabcda0c087c96ffda99e1f2a74fcff0cf"
   end
 
   name "CC Controller"
