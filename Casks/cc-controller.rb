96516a1614516d9fa99e31d2f7179f0dda5f5d3b
Daniel Pineda <269362667+kitifica-max@users.noreply.github.com>
2026-08-14 16:40:48 -0600

    chore: fix SHA-256 checksums for v1.5.1 DMGs

--- Casks/cc-controller.rb
+++ Casks/cc-controller.rb
@@ -3,12 +3,12 @@ cask "cc-controller" do
 
   on_arm do
     url "https://github.com/kitifica-max/cc-controller/releases/download/v1.5.1/CC.Controller-1.5.1-arm64.dmg"
-    sha256 "472739a1bfe8aea23e51f8ad36d52db1f42bca933739d801f65dea0b7af71c34"
+    sha256 "5d34d5f7585fc6ba22d11cd98fe63f5464016d602ab06ee4207d3a0f501cb560"
   end
 
   on_intel do
     url "https://github.com/kitifica-max/cc-controller/releases/download/v1.5.1/CC.Controller-1.5.1.dmg"
-    sha256 "4b57cc795976296a707f80c03dc7e44cdbb5446e70ceaf2c0b04b00eb41a789c"
+    sha256 "b839de219fead31ca23e79bf4581d954711366594992eb782d32d8d40d9f340d"
   end
 
   name "CC Controller"
