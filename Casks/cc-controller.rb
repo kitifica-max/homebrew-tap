fd3fd59528376a8434a02e04db1ea388788988bd
CC Controller CI <ci@kitifica.com>
2026-08-18 02:59:50 +0000

    chore: bump cc-controller to v1.7.11

--- Casks/cc-controller.rb
+++ Casks/cc-controller.rb
@@ -1,14 +1,14 @@
 cask "cc-controller" do
-  version "1.7.8"
+  version "1.7.11"
 
   on_arm do
-    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.7.8/CC.Creator-1.7.8-arm64.dmg"
-    sha256 "32a6e915d409fd7b55ecc2f6310a4cb4962349c6e45b69f2104e378a6664127d"
+    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.7.11/CC.Creator-1.7.11-arm64.dmg"
+    sha256 "b5bc4c8dcf29095a000f4c04db0a8051c0945aeb7bff961671a6c2ed88c64f6e"
   end
 
   on_intel do
-    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.7.8/CC.Creator-1.7.8.dmg"
-    sha256 "b1f0d515c3caa3f24cf0abf71c388ec21d4195ba65bdac4cb46a01d630405274"
+    url "https://github.com/kitifica-max/cc-controller/releases/download/v1.7.11/CC.Creator-1.7.11.dmg"
+    sha256 "3b5345eb85cbc27069412caf13af8dca8058c138fe585f3793d759a9abbdb2a1"
   end
 
   name "CC Creator"
